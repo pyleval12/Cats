cce13c3b13d7fb4e36feec80aab362e6f63556d0
Александр <pylev5868@gmail.com>
2024-11-24 15:35:50 +0300

    Картинки теперь открываются в новом окне

--- Cataas.py
+++ Cataas.py
@@ -16,13 +16,16 @@ def load_image(url):
         print(f"Произошла ошибка: {e}")
         return None
 
-def set_image():
+def open_new_window():
     #global url
     img = load_image(url)
     if img:
-        label.config(image=img)
+        new_window = Toplevel()
+        new_window.title("Картинка с котом")
+        new_window.geometry("600x480")
+        label = Label(new_window,image=img)
+        label.pack()
         label.image = img
-
 def exit():
     window.destroy()
 
@@ -35,19 +38,16 @@ window.config(menu=main_menu)
 file_menu = Menu(main_menu, tearoff = 0)
 
 main_menu.add_cascade(label="Файл", menu =file_menu)
-file_menu.add_command(label="Загрузить фото", command = set_image)
+file_menu.add_command(label="Загрузить фото", command = open_new_window)
 file_menu.add_separator()
 file_menu.add_command(label="Выход", command = exit)
 
 
 # update_button = Button(text="Обновить", command = set_image)
 # update_button.pack()
-label = Label()
-label.pack()
+
 
 url = "https://cataas.com/cat"
-
-set_image()
 
 
 window.mainloop()
