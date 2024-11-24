d54d3267aa2bb4bad5506e3cdada72416b276ad7
Александр <pylev5868@gmail.com>
2024-11-24 16:08:04 +0300

    Добавлено получение изображений по тэгу, а также без тэга

--- Cataas.py
+++ Cataas.py
@@ -17,8 +17,10 @@ def load_image(url):
         return None
 
 def open_new_window():
-    #global url
-    img = load_image(url)
+    tag = tag_entry.get()
+    url_tag = f"https://cataas.com/cat/{tag}" if tag else "https://cataas.com/cat"
+    img = load_image(url_tag)
+
     if img:
         new_window = Toplevel()
         new_window.title("Картинка с котом")
@@ -33,6 +35,11 @@ window = Tk()
 window.title("Cats!")
 window.geometry("600x520")
 
+tag_entry = Entry()
+tag_entry.pack()
+load_button = Button(text="Загрузить по тэгу", command = open_new_window)
+load_button.pack()
+
 main_menu = Menu(window)
 window.config(menu=main_menu)
 file_menu = Menu(main_menu, tearoff = 0)
@@ -43,8 +50,6 @@ file_menu.add_separator()
 file_menu.add_command(label="Выход", command = exit)
 
 
-# update_button = Button(text="Обновить", command = set_image)
-# update_button.pack()
 
 
 url = "https://cataas.com/cat"
